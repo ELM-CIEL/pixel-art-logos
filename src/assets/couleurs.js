707485afe4654b0f638ee6a8fc7905900adeb4a9
ELM-CIEL <moahmed.elmokhtari26@gmail.com>
2026-02-13 17:36:04 +0100

    feat : ajout couleurs.js et matrices.js
    + ajout pixel art pour logos
    + ajout palette couleur pour pixel art

---
+++ src/assets/couleurs.js
@@ -0,0 +1,35 @@
+export const couleurs = {
+  react: {
+    0: "transparent",
+    1: "#61DAFB",
+  },
+  javascript: {
+    0: "#F7DF1E",
+    1: "#000000",
+  },
+  nextjs: {
+    0: "transparent",
+    1: "#000000",
+  },
+  nodejs: {
+    0: "transparent",
+    1: "#339933",
+  },
+  tailwind: {
+    0: "transparent",
+    1: "#339933",
+  },
+  python: {
+    0: "transparent",
+    1: "#3776AB",
+    2: "#FFD43B",
+  },
+  mysql: {
+    0: "transparent",
+    1: "#4479A1",
+  },
+  mongodb: {
+    0: "transparent",
+    1: "#47A248",
+  },
+};
